d7a0eb10fa368c38f41c6f746158c42fa14be1ea
yzl <niania_yu@163.com>
2019-04-22 15:39:41 +0800

    Demo_hello will restart every 2s

--- esp-idf/examples/get-started/hello_world/main/hello_world_main.c
+++ esp-idf/examples/get-started/hello_world/main/hello_world_main.c
@@ -32,7 +32,7 @@ void app_main()
 
     for (int i = 10; i >= 0; i--) {
         printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
     printf("Restarting now.\n");
     fflush(stdout);
